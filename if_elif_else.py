0688e4c5d204ea09c8e39173d0e3014b97dc8f0f
Ankit Kumar <71585377+Anky209e@users.noreply.github.com>
2020-11-18 21:51:15 +0530

    If,Elif & else statements

---
+++ if_elif_else.py
@@ -0,0 +1,15 @@
+#num1 = 12
+#num2 = 13
+#if num1<num2:
+#    #print("num1 is greater than num 2")
+# var1 = int(input("Enter your No 1 :\n"))
+# var2 = int(input("Enter your No 2 :\n"))
+# if var1>var2:
+    # print("no1 is greater than no 2\n")
+# else:
+    # print("no2 is lesser than no1")
+# l1 = [1,2,3,4]
+# if 1  in l1:
+    # print("YES")
+# elif 7 not in l1:
+    # print("NO")
