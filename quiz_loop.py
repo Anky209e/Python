b2799b6f9b8eb90a80ecde3f2ddaf9c6c44c2ccc
Ankit Kumar <71585377+Anky209e@users.noreply.github.com>
2020-11-19 11:42:56 +0530

    Loop practice quiz of entering no above 100

---
+++ quiz_loop.py
@@ -0,0 +1,9 @@
+while (True):
+    num1 = int(input("Enter your no:\n"))
+    if num1 >100:
+        print("Succeded")
+        break
+    
+    elif num1<100:
+        print("Try again")
+    continue
