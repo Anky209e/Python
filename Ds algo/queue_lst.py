d54d3cfeddec2bc5ffdd9c100b0251ffc6c5e8df
Ankit Kumar <nasaankit01@gmail.com>
2021-10-22 06:00:19 +0530

    Queue using linked list

--- Ds algo/queue_lst.py
+++ Ds algo/queue_lst.py
@@ -5,18 +5,42 @@ class Node:
 class Queue:
     def __init__(self):
         self.head = None
-    def add(self,data):
-        node_d = Node(data)
-
-
+        self.tail = None
+    def isEmpty(self):
+        return self.head == None
+    
+    #adding item in queue
+    def Enqueue(self,data):
+        temp = Node(data)
+        if self.tail == None:
+            self.head = self.tail = temp
+            return
+        self.tail.next = temp
+        self.tail = temp
+    
+    #removing item from queue
+    def Dequeue(self):
+        if self.isEmpty():
+            raise Exception("Queue is already empty.")
+        temp = self.head
+        self.head = temp.next
+        if (self.head == None):
+            self.tail = None
+    #showing queue
     def show(self):
         temp = self.head
         while(temp):
             print(temp.data)
             temp = temp.next
 
-if __name__ == "__main__":
-    queue = Queue()
-    for  i in range(2,3):
-        queue.add(i)
-queue.show()
+my_queue = Queue()
+my_queue.Enqueue(10)
+my_queue.Enqueue(111)
+my_queue.Enqueue(12)
+my_queue.Enqueue(13)
+my_queue.Enqueue(14)
+my_queue.show()
+my_queue.Dequeue()
+my_queue.Dequeue()
+print("------------")
+my_queue.show()
