2d68b6e4160f6b72418434766f30a9f89ec996a1
Ankit Kumar <nasaankit01@gmail.com>
2021-10-22 06:00:33 +0530

    queue using array

--- Ds algo/queues.py
+++ Ds algo/queues.py
@@ -8,7 +8,7 @@ class Queue:
         self.arr.append(data)
         return self.arr
     def dqueue(self):
-        self.arr.remove(0)
+        self.arr.pop(0)
         return self.arr
     def size(self):
         return len(self.arr)
@@ -16,10 +16,11 @@ class Queue:
 
 
 queu = Queue()
-for i in range(0,10):
+for i in range(9,15):
     queu.enqueue(i)
 queu.show()
 queu.enqueue(123)
 queu.show()
+queu.dqueue()
 queu.dqueue()
 queu.show()
