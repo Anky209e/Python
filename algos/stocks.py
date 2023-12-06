cac66df8b23deaedae2b068c38a7b6ab29ff159d
Ankit Kumar <nasaankit01@gmail.com>
2023-12-06 18:33:29 +0530

    Update stocks.py

--- algos/stocks.py
+++ algos/stocks.py
@@ -20,4 +20,18 @@ def maxProfit(prices):
 
     return max_profit
 
+def maxProfit_2(prices) -> int:
+        min_price = prices[0]
+        max_profit = 0
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            
+            profit = price - min_price
+
+            max_profit = max(max_profit,profit)
+        return max_profit
+                        
+
 print(maxProfit(prices))
